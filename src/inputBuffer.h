3dd64c564573daa253a6d9a85fcb5b9ee0f0d7c6
琉忆 <80893434+Ly403@users.noreply.github.com>
2022-10-05 19:59:18 +0800

    加入了预处理功能
    预处理类打开文件，读入缓冲区，去除注释内容

---
+++ src/inputBuffer.h
@@ -0,0 +1,33 @@
+#pragma once
+#include <string>
+#include <fstream>
+#include <iostream>
+using namespace std;
+
+static string::size_type min(string::size_type a, string::size_type b, string::size_type c);
+
+enum class InputState{
+	CORRECT,END_OF_FILE,NO_CLOSED_COMMENT,ERROR_COMMENT
+};
+
+class InputBuffer
+{
+public:
+	InputBuffer();
+	InputBuffer(const string& path);
+	InputState readline();
+	InputState readAll();
+	bool getIsExegesis();
+	int getLineNumber();
+	void pop(string& s);
+	void reset();
+	bool open(const string& path);
+	const string& getBuffer() const;
+	~InputBuffer();
+private:
+	string buffer;
+	string path;
+	ifstream fin;
+	int line;
+	bool isExegesis;//ÊÇ·ñÔÚ×¢ÊÍÌ¬
+};
