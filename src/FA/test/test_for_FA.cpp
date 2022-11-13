04f299b74838b335646dd0b6f9828e92fca26e6d
whistleH <2263696191@qq.com>
2022-11-13 21:41:58 +0800

    整理代码结构

--- src/FA/test/test_for_FA.cpp
+++ src/FA/test/test_for_FA.cpp
@@ -1,30 +1,64 @@
 #include <iostream>
 #include <string>
 #include "FA.h"
+#include "config.h"
 using namespace std;
 
-int main()
+config con;
+
+void save(FA& dfa)
 {
-	/*string reg = "a*|b*";
-	FA fa(reg);*/
+    string para = "nothing here";
+	FA fa(para, READ_SYMBOLTABLE);
+	dfa = fa.toDFA();
+    dfa.saveDFA("dfamodel");
+}
 
-	string filename = "regex.txt";
-	FA fa(filename, READ_FILE_BY_LINE);
-    // fa.showFA();
+void load(FA& dfa)
+{
+    dfa.loadDFA("dfamodel");
+}
 
-	FA dfa = fa.toDFA();
-    dfa.showFA();
+int main(int argc,char** argv)
+{
+    
+    FA dfa;
+    // string para = "nothing here";
+	// FA fa(para, READ_SYMBOLTABLE);
+	// dfa = fa.toDFA();
+    // dfa.saveDFA("dfamodel");
+    if(argc <2)
+    {
+        cout<<"You need give 0 for save,1 for load";
+        exit(-1);
+    }
+    if(string(argv[1]) == "1"){
+        load(dfa);
+    }else if(string(argv[1]) == "0"){
+        save(dfa);
+    }
+
     int sys_idx, err_idx;
-    string test[] = {
-        "inti", 
-        "int",
-        "int  a++  ",
-        "main()",
-        "main"
-    };
-    for(auto ss : test){
+    int ParaNum = 2;
+    while(ParaNum < argc)
+    {
+        string ss(argv[ParaNum]);
+        ++ParaNum;
         cout << ss << ": " << endl;
         dfa.checkStr(ss, sys_idx, err_idx);
     }
+    // string test[] = {
+    //     "inti", 
+    //     "int",
+    //     "int  a++  ",
+    //     "main()",
+    //     "main",
+    //     //"\"12324342\"",
+    //     "123"
+    // };
+    // for(auto ss : test){
+    //     cout << ss << ": " << endl;
+    //     dfa.checkStr(ss, sys_idx, err_idx);
+    // }
 	return 0;
 }
